09ab59e2a6b74906ff3e61be6f6f837b38e6a661
João Vitor  Cavalcante <joaocavalcante@MacBook-Air-de-Joao.local>
2024-07-24 19:07:21 -0300

    Feat: JWT-Auth configurado

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,18 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('caminhao', 'App\Http\Controllers\CaminhaoController');
-Route::apiResource('motorista', 'App\Http\Controllers\MotoristaController');
-Route::apiResource('viagem', 'App\Http\Controllers\ViagemController');
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'v1'
+], function($router) {
+    Route::apiResource('caminhao', 'App\Http\Controllers\CaminhaoController');
+    Route::apiResource('motorista', 'App\Http\Controllers\MotoristaController');
+    Route::apiResource('viagem', 'App\Http\Controllers\ViagemController');
+    
+    Route::post('/me', 'App\Http\Controllers\AuthController@me');
+    Route::post('/refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
+});
+
+
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
